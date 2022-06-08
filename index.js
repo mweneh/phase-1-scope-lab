6f9841176f7e0a6351bed37d8cb6395548fa92ec
David Ongaro <davidongaroh@gmil.com>
2022-06-08 13:03:37 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+var customerName= `bob`
+const leastFavoriteCustomer = 'Sean'
+
+var returnsCustomerName= function(){
+    return customerName
+}
+
+var upperCaseCustomerName= function(){
+    return customerName=customerName.toUpperCase()
+
+}
+var  setBestCustomer= function(){
+    bestCustomer='not bob'
+    return bestCustomer
+
+}
+var overwriteBestCustomer = function(){
+     bestCustomer='maybe bob'
+    return bestCustomer
+}
+ changeLeastFavoriteCustomer= function(){
+     leastFavoriteCustomer= 'Chris'
+    
+   
+}
